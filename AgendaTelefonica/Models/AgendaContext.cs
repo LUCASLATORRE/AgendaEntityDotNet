2956cd7bae7dddea38168e806d99026da14e7dc1
Lucas Latorre <lucaslatorre@gmail.com>
2019-03-22 16:01:49 -0300

    first commit

---
+++ AgendaTelefonica/Models/AgendaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AgendaTelefonica.Models
+{
+    public class AgendaContext : DbContext 
+    {
+        public AgendaContext() : base("DbLoja")
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Consultor> Consultores { get; set; }
+        public DbSet<Telefone> Telefones { get; set; }
+    }
+}
